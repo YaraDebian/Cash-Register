869fd0d72f0748dd6b90e2155e86616e0fd5d8b7
YaraDebian <yaradebian1@gmail.com>
2024-01-18 21:46:49 +0200

    fix seeds to remove empty cart

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
 # encoding: UTF-8
 
+CartItem.destroy_all
 Product.destroy_all
+Cart.destroy_all
+
 Product.create(name: 'Green Tea 🍵', code: 'GR1', price: 3.11)
 Product.create(name: 'Strawberries 🍓', code: 'SR1', price: 5.00)
 Product.create(name: 'Coffee ☕️', code: 'CF1', price: 11.23)
+Cart.create
+
